30742c27d8fec199e4d6e1f965193b5377f4e816
djfightking <durronjaemyers@gmail.com>
2024-04-29 21:52:23 -0500

    lot's of new games and more
    better looking password page games better better looking and improved password html

--- Data/js/pass.js
+++ Data/js/pass.js
@@ -2,20 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const input = document.getElementById("categoryInput");
     let enteredDigits = '';
 
-    input.addEventListener("input", function() {
-        enteredDigits += input.value.charAt(input.value.length - 1);
-
-        if (enteredDigits.length === 5) {
-            if (enteredDigits === "91121") {
-                // Redirect to another HTML page
-                window.location.href = "Main.html";
-            } else {
-                alert("Password wrong. Please try again.");
-                enteredDigits = '';
-            }
-        } else if (enteredDigits.length > 5) {
-            alert("Password must be exactly 5 characters long.");
-            enteredDigits = '';
+    input.addEventListener("keypress", function(event) {
+        if (event.key === 'Enter') {
+            performSearch();
+        } else {
+            enteredDigits += event.key;
         }
     });
+
+    document.getElementById("searchButton").addEventListener("click", function() {
+        performSearch();
+    });
+
+    function performSearch() {
+        if (enteredDigits === "91121") {
+            // Redirect to another HTML page
+            window.location.href = "Main.html";
+        } else {
+            alert("Password wrong. Please try again.");
+            enteredDigits = '';
+        }
+    }
 });
