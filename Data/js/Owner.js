f9ae1b8a7e17c1928a075315fb1af2ecb347bc80
djfightking <durronjaemyers@gmail.com>
2024-05-05 17:35:09 -0500

    a

--- Data/js/Owner.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("button");
-    if(button) {
-        button.addEventListener("click", function() {
-            window.location.href = "Data/code-space.html"; // Updated URL to navigate to Data/code-space.html
-        });
-    }
-});
